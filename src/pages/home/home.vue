91c0735efe0fcf7cac7e7be32a05fddb4a19edd4
thomson <tangshiyang@loyowo.net>
2021-12-24 18:21:52 +0800

    主页部分布局

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -17,9 +17,42 @@
         <view class="swiper-item-red">C</view>
       </swiper-item>
     </swiper>
-    <view class="user-info-view"></view>
-    <view class="order-view"></view>
-    <view class="user-new-view"></view>
+    <view class="user-info-view">
+      <view class="user-info-content">
+        <img src="../static/img/home_tab_unselect.png" class="user-info-content-img" />
+        <view class="user-info-content-body">
+          <text class="user-info-content-body-name">昵称</text>
+          <text class="user-info-content-body-time">永久有效</text>
+        </view>
+      </view>
+      <view class="user-info-coupon-content">
+        <view class="user-info-coupon">
+          <text>112.5</text>
+          <text>积分</text>
+        </view>
+        <view class="user-info-coupon-line" />
+        <view class="user-info-coupon">
+          <text>112.2</text>
+          <text>优惠劵</text>
+        </view>
+      </view>
+    </view>
+    <view class="order-view">
+      <view class="order-view-head">
+        <view class="order-view-head-item">
+          <img src="../static/img/home_tab_unselect.png" class="order-view-head-item-img" />
+          <text>自提</text>
+        </view>
+        <view class="order-view-head-item">
+          <img src="../static/img/home_tab_unselect.png" class="order-view-head-item-img" />
+          <text>外卖</text>
+        </view>
+      </view>
+      <view class="order-view-bottom"></view>
+    </view>
+    <view class="user-new-view">
+      <text>新人专享</text>
+    </view>
     <view class="product-new-view"></view>
   </view>
 </template>
@@ -67,13 +100,86 @@ export default {
   height: 137rpx;
   margin-top: -68.5rpx;
 
+  /* padding-left: 80rpx;
+  padding-right: 80rpx; */
+
+  display: flex;
+  flex-direction: row;
+
+  justify-content: space-between;
+
+  border-radius: 10rpx;
+  background-color: gold;
+
+  z-index: 10;
+}
+.user-info-content {
+  display: flex;
+  flex-direction: row;
+
+  align-content: center;
+  align-items: center;
+
+  margin-left: 46rpx;
+}
+
+.user-info-content-img {
+  height: 33px;
+  width: 33px;
+
+  border-radius: 15px;
+}
+
+.user-info-content-body {
   display: flex;
   flex-direction: column;
 
-  border-radius: 10rpx;
-  background-color: blue;
+  text-align: left;
+  margin-left: 20rpx;
+}
+.user-info-content-body-name {
+  font-size: 11px;
+  font-weight: 400;
+  color: #000000;
+}
 
-  z-index: 10;
+.user-info-content-body-time {
+  font-size: 10px;
+  font-weight: 300;
+  color: #999999;
+
+  margin-top: 9rpx;
+}
+
+.user-info-coupon-content {
+  display: flex;
+  flex-direction: row;
+
+  align-content: center;
+  align-items: center;
+
+  margin-right: 46rpx;
+}
+
+.user-info-coupon {
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  text-align: center;
+}
+.user-info-coupon text {
+  font-size: 12px;
+  font-weight: 400;
+  color: #000000;
+}
+.user-info-coupon-line {
+  height: 80%;
+  widows: 5px;
+  background-color: red;
+
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .order-view {
   width: 90%;
@@ -85,6 +191,40 @@ export default {
 
   border-radius: 10rpx;
   background-color: blue;
+
+  justify-content: center;
+  justify-items: center;
+}
+
+.order-view-head {
+  display: flex;
+  flex-direction: row;
+  height: 300rpx;
+}
+.order-view-head-item {
+  width: 50%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  align-content: center;
+}
+
+.order-view-head-item-img {
+  width: 22px;
+}
+.order-view-head-item text {
+  margin-top: 22rpx;
+  font-size: 18px;
+  font-weight: 500;
+  color: #000000;
+}
+
+.order-view-bottom {
+  display: flex;
+  flex-direction: row;
 }
 
 .user-new-view {
@@ -97,6 +237,20 @@ export default {
 
   border-radius: 10rpx;
   background-color: blue;
+}
+.user-new-view text {
+  font-size: 18px;
+  font-weight: 500;
+  color: #000000;
+
+  margin-left: 20rpx;
+  margin-top: 20rpx;
+}
+.product-new-view-head {
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
 }
 .product-new-view {
   width: 90%;
